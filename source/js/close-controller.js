b597565d99b81e6b389b9ead3af9772bc91f9d03
jerryngton <jerryngtond@gmail.com>
2018-02-18 10:15:07 +0300

    no js

--- source/js/close-controller.js
+++ source/js/close-controller.js
@@ -1,6 +1,7 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
+navMain.classList.remove("main-nav--links-nojs");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--links-hidden")) {
